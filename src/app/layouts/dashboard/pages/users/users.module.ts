ea683b91f7a4b034b8bde361250019f2d753ab67
GwynethS <gwynethsegura@gmail.com>
2024-01-22 09:25:16 -0500

    feat: se creó el componente y módulo users

---
+++ src/app/layouts/dashboard/pages/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
