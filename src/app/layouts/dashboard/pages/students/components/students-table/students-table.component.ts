2b1569c71e20b972d1d6c140698c24346a2f62fe
GwynethS <gwynethsegura@gmail.com>
2024-01-22 09:31:56 -0500

    feat: se creó el componente students-table

---
+++ src/app/layouts/dashboard/pages/students/components/students-table/students-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-table',
+  templateUrl: './students-table.component.html',
+  styleUrl: './students-table.component.scss'
+})
+export class StudentsTableComponent {
+
+}
--- src/app/layouts/dashboard/pages/students/students.module.ts
+++ src/app/layouts/dashboard/pages/students/students.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsComponent } from './students.component';
 import { StudentsFormComponent } from './components/students-form/students-form.component';
+import { StudentsTableComponent } from './components/students-table/students-table.component';
 
 
 
 @NgModule({
   declarations: [
     StudentsComponent,
-    StudentsFormComponent
+    StudentsFormComponent,
+    StudentsTableComponent
   ],
   imports: [
     CommonModule
