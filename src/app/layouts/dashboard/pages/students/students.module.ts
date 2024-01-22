5dba95a5fba8a7b90a5e5780dc90e4b6bdb3b021
GwynethS <gwynethsegura@gmail.com>
2024-01-22 09:30:45 -0500

    fea: se creó el módulo y componente students

---
+++ src/app/layouts/dashboard/pages/students/students.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentsComponent } from './students.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentsModule { }
