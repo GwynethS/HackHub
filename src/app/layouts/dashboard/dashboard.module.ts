ce165162eaabf064082fe307a2e5d1fe2d1aed73
GwynethS <gwynethsegura@gmail.com>
2024-01-22 09:21:31 -0500

    feat: se creó el componente toolbar

--- src/app/layouts/dashboard/dashboard.module.ts
+++ src/app/layouts/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    SidebarComponent
+    SidebarComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule
