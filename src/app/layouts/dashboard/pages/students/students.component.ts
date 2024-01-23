cb36fcb17736d9266ce7dd04ff63a34ba97bb367
GwynethS <gwynethsegura@gmail.com>
2024-01-22 20:45:54 -0500

    feat: función editar estudiante

--- src/app/layouts/dashboard/pages/students/students.component.ts
+++ src/app/layouts/dashboard/pages/students/students.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from './models/student';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-students',
@@ -7,13 +8,9 @@ import { Student } from './models/student';
   styleUrl: './students.component.scss'
 })
 export class StudentsComponent {
+  studentForm: FormGroup;
 
-  studentSelected: Student = {
-    id: 0,
-    firstName: '',
-    lastName: '',
-    email: ''
-  }
+  studentSelected: Student | null = null;
 
   students: Student[] = [
     {
@@ -23,16 +20,47 @@ export class StudentsComponent {
       email: 'gwyneth@gmail.com'
     }
   ]
+
+  constructor(private fb: FormBuilder){
+    this.studentForm = this.fb.group({
+      firstName: this.fb.control('', [
+        Validators.required, 
+        Validators.minLength(2), 
+        Validators.pattern('[a-zA-Z]*')]),
+      lastName: this.fb.control('', [
+        Validators.required, 
+        Validators.minLength(2), 
+        Validators.pattern('[a-zA-Z]*')]),
+      email: this.fb.control('', [
+        Validators.required, 
+        Validators.email]),
+    })
+  }
+
   onStudentSubmitted(ev: Student){
     this.students = [...this.students, {...ev, id: this.students.length + 1}]
   }
 
   onEditStudent(ev: Student){
-    
+    this.studentSelected = ev;
+    this.studentForm.patchValue(this.studentSelected);
+  }
+
+  onSaveEditStudent(){
+    if(this.studentForm.invalid || this.studentSelected === null){
+      this.studentForm.markAllAsTouched();
+    }else{
+      this.students = this.students.map((student) => student.id === this.studentSelected?.id ? {...this.studentSelected, ...this.studentForm.value} : student );
+      this.studentSelected = null;
+    }
+  }
+
+  onCancelEditStudent(){
+    this.studentSelected = null;
+    this.studentForm.reset();
   }
 
   onDeleteStudent(id: number): void{
     this.students = this.students.filter((student) => student.id !== id);
-    console.log('delete');
   }
 }
