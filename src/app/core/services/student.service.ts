0b993783aa09d4c4ebc13aa8d248b608cfc1b868
GwynethS <gwynethsegura@gmail.com>
2024-02-02 17:24:59 -0500

    feat: se creó el servicio de estudiantes con los métodos agregar, eliminar y actualizar

---
+++ src/app/core/services/student.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Student } from '../../layouts/dashboard/pages/students/models/student';
+import { delay, finalize, of } from 'rxjs';
+
+let students: Student[] = [
+  {
+    id: 1,
+    firstName: 'Gwyneth',
+    lastName: 'Segura',
+    email: 'gwyneth@gmail.com'
+  }
+]
+
+@Injectable()
+export class StudentsService {
+
+  constructor() { }
+
+  getStudents(){
+    return of(students).pipe(
+      delay(500)
+    );
+  }
+
+  addStudent(studentData: Student){
+    students = [...students, {...studentData, id: students.length++}];
+
+    return this.getStudents();
+  }
+
+  deleteStudentById(id: number){
+    students = students.filter((student) => student.id !== id);
+
+    return this.getStudents();
+  }
+
+  updateStudent(id: number, updateData: Student){
+    students = students.map((student) => student.id === id ? {...student, ...updateData} : student);
+
+    return this.getStudents();
+  }
+}
