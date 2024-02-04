fb66146e76ac58ea368e3d51f47873ab8682e65a
GwynethS <gwynethsegura@gmail.com>
2024-02-04 01:37:49 -0500

    feat: se añadieron los métodos crear, editar y eliminar en el servicio enrollment

--- src/app/layouts/dashboard/pages/enrollment/enrollment.service.ts
+++ src/app/layouts/dashboard/pages/enrollment/enrollment.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Enrollment } from './models/enrollment';
+import { delay, of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+let enrollments: Enrollment[] = [
+  {
+    id: 1,
+    studentId: 1,
+    courseId: 1,
+    enrollmentDate: new Date()
+  }
+];
+
+@Injectable()
 export class EnrollmentService {
 
   constructor() { }
+
+  getEnrollments(){
+    return of(enrollments).pipe(
+      delay(500)
+    )
+  }
+
+  getEnrollmentById(id: number){
+    return of(enrollments.find((enrollment) => enrollment.id === id)).pipe(delay(500));
+  }
+
+  createEnrollment(enrollmentData: Enrollment){
+    enrollments = [...enrollments, {...enrollmentData, id: enrollments.length + 1}];
+
+    return this.getEnrollments();
+  }
+
+  deleteEnrollmentById(id: number){
+    enrollments = enrollments.filter((enrollment) => enrollment.id !== id);
+
+    return this.getEnrollments();
+  }
+
+  updateEnrollment(id: number, updateData: Enrollment){
+    enrollments = enrollments.map((enrollment) => enrollment.id === id ? {...enrollment, ...updateData} : enrollment);
+
+    return this.getEnrollments();
+  }
 }
---
+++ src/app/layouts/dashboard/pages/enrollment/models/enrollment.ts
@@ -0,0 +1,6 @@
+export interface Enrollment{
+  id: number,
+  studentId: number,
+  courseId: number,
+  enrollmentDate: Date
+}
