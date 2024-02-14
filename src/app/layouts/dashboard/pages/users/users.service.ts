5aacfbca9dbf55752d269f846909b78f0b9f2811
GwynethS <gwynethsegura@gmail.com>
2024-02-13 23:17:40 -0500

    feat: se implementaron las peticiones a través de API Rest en el servicio de usuario

--- src/app/layouts/dashboard/pages/users/users.service.ts
+++ src/app/layouts/dashboard/pages/users/users.service.ts
@@ -1,54 +1,62 @@
 import { Injectable } from '@angular/core';
 import { User } from './models/user';
-import { delay, of } from 'rxjs';
-
-let users: User[] = [
-  {
-    id: 1,
-    firstName: 'Lyu',
-    lastName: 'Segura',
-    email: 'lyu@gmail.com',
-    password: '123456',
-    role: 'ADMIN',
-    token: 'ksjdfhkjah'
-  },
-  {
-    id: 2,
-    firstName: 'Jean',
-    lastName: 'Achamizo',
-    email: 'jean@gmail.com',
-    password: '123456',
-    role: 'USER',
-    token: 'ladkajlsk'
-  }
-]
+import { Observable, delay, mergeMap, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable()
 export class UsersService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  generateString(length: number) {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
 
-  getUsers(){
-    return of(users).pipe(
-      delay(500)
-    );
+    return result;
   }
 
-  createUser(userData: User){
-    users = [...users, {...userData, id: users.length + 1}];
-
-    return this.getUsers();
+  getUsers() {
+    return this.httpClient.get<User[]>(`${environment.apiURL}/users`);
   }
 
-  deleteUserById(id: number){
-    users = users.filter((user) => user.id !== id);
+  getUserById(id: number): Observable<User | undefined> {
+    return this.httpClient.get<User>(`${environment.apiURL}/users/${id}`);
+  }
 
-    return this.getUsers();
+  createUser(userData: User) {
+    return this.httpClient
+      .post<User>(`${environment.apiURL}/users`, {
+        ...userData,
+        token: this.generateString(10),
+      })
+      .pipe(mergeMap(() => this.getUsers()));
+  }
+
+  deleteUserById(id: number) {
+    return this.httpClient
+      .delete<User>(`${environment.apiURL}/users/${id}`)
+      .pipe(mergeMap(() => this.getUsers()));
   }
 
   updateUser(id: number, updateData: User){
-    users = users.map((user) => user.id === id ? {...user, ...updateData} : user);
+    return this.getUserById(id).pipe(
+      mergeMap((existingUser) => {
+        const updatedUser: User = {
+          ...existingUser,
+          ...updateData,
+        };
 
-    return this.getUsers();
+        return this.httpClient.put<User>(
+          `${environment.apiURL}/users/${id}`,
+          updatedUser
+        );
+      }),
+      mergeMap(() => this.getUsers())
+    );
   }
 }
