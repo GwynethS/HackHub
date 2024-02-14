12280a2361a5cde75bc3f837505a955556ef8790
GwynethS <gwynethsegura@gmail.com>
2024-02-14 18:47:03 -0500

    feat: se realizaron las pruebas unitarias iniciales para HomeComponent

--- src/app/layouts/dashboard/pages/home/home.component.spec.ts
+++ src/app/layouts/dashboard/pages/home/home.component.spec.ts
@@ -1,23 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [HomeComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.createComponent(HomeComponent).componentInstance;
   });
 
-  it('should create', () => {
+  it('HomeComponent debe estar definido', () => {
     expect(component).toBeTruthy();
   });
 });
