9ee82729c6a3dcc0c8c96395a8a7f3bb0892bb46
GwynethS <gwynethsegura@gmail.com>
2024-02-14 18:42:33 -0500

    feat: se realizaron las pruebas unitarias para LoginComponente

--- src/app/layouts/auth/pages/login/login.component.spec.ts
+++ src/app/layouts/auth/pages/login/login.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { SharedModule } from '../../../../shared/shared.module';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { MockProvider } from 'ng-mocks';
+import { AuthService } from '../../auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [SharedModule, ReactiveFormsModule],
+      providers: [MockProvider(AuthService)]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.createComponent(LoginComponent).componentInstance;
   });
 
-  it('should create', () => {
+  it('LoginComponent debe estar definido', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('El email y la contraseña del formulario deben ser campos requeridos', () => {
+    expect(component.loginForm.get('email')?.hasValidator(Validators.required)).toBeTrue();
+    expect(component.loginForm.get('password')?.hasValidator(Validators.required)).toBeTrue();
+  });
+
+  it('Se debe evitar el envío de un formulario inválido y marcar los campos con error', () => {
+    component.loginForm.patchValue({
+      email: '',
+      password: ''
+    })
+
+    expect(component.loginForm.invalid).toBeTrue();
+
+    const spyOnMarkAllAsTouched = spyOn(component.loginForm, 'markAllAsTouched');
+
+    component.onSubmit();
+    expect(spyOnMarkAllAsTouched).toHaveBeenCalled();
   });
 });
