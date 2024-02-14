af44b504e94e0c7459938c9d113984069fb64b9b
GwynethS <gwynethsegura@gmail.com>
2024-02-13 20:20:59 -0500

    feat: se agregaron las rutas a los módulos users y home

--- src/app/layouts/dashboard/dashboard-routing.module.ts
+++ src/app/layouts/dashboard/dashboard-routing.module.ts
@@ -1,32 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersModule } from './pages/users/users.module';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then((m) => m.UsersModule),
+  },
+  {
     path: 'students',
     loadChildren: () =>
-      import('./pages/students/students.module').then(
-        (m) => m.StudentsModule
-      )
+      import('./pages/students/students.module').then((m) => m.StudentsModule),
   },
   {
     path: 'courses',
     loadChildren: () =>
-      import('./pages/courses/courses.module').then(
-        (m) => m.CoursesModule
-      )
+      import('./pages/courses/courses.module').then((m) => m.CoursesModule),
   },
   {
     path: 'enrollment',
     loadChildren: () =>
       import('./pages/enrollment/enrollment.module').then(
         (m) => m.EnrollmentModule
-      )
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
