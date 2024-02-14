11f92fbd19ddf925ce99507ebe63b3e04b329a46
GwynethS <gwynethsegura@gmail.com>
2024-02-13 19:05:08 -0500

    Feat: se creó el componente login

--- src/app/layouts/auth/auth.module.ts
+++ src/app/layouts/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
