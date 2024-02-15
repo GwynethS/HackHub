d3f3e125338d463640a5c3e41fadb72d484fd461
GwynethS <gwynethsegura@gmail.com>
2024-02-14 23:03:04 -0500

    feat: se realizaron las pruebas unitarias para el componente de users-dialog

--- src/app/layouts/dashboard/pages/users/components/users-dialog/users-dialog.component.spec.ts
+++ src/app/layouts/dashboard/pages/users/components/users-dialog/users-dialog.component.spec.ts
@@ -4,6 +4,7 @@ import { UsersDialogComponent } from './users-dialog.component';
 import { SharedModule } from '../../../../../../shared/shared.module';
 import { MockProvider } from 'ng-mocks';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Validators } from '@angular/forms';
 
 describe('UsersDialogComponent', () => {
   let component: UsersDialogComponent;
@@ -21,4 +22,36 @@ describe('UsersDialogComponent', () => {
   it('UsersDialogComponent debe estar definido', () => {
     expect(component).toBeTruthy();
   });
+
+  it('El firstNme, lastName, email, password y role del formulario deben ser campos requeridos', () => {
+    expect(component.userForm.get('firstName')?.hasValidator(Validators.required)).toBeTrue();
+    expect(component.userForm.get('lastName')?.hasValidator(Validators.required)).toBeTrue();
+    expect(component.userForm.get('email')?.hasValidator(Validators.required)).toBeTrue();
+    expect(component.userForm.get('password')?.hasValidator(Validators.required)).toBeTrue();
+    expect(component.userForm.get('role')?.hasValidator(Validators.required)).toBeTrue();
+  });
+
+  it('Se debe evitar el envío de un formulario inválido y marcar los campos con error', () => {
+    component.userForm.patchValue({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      role: ''
+    })
+
+    expect(component.userForm.invalid).toBeTrue();
+
+    const spyOnMarkAllAsTouched = spyOn(component.userForm, 'markAllAsTouched');
+
+    component.onCreate();
+    expect(spyOnMarkAllAsTouched).toHaveBeenCalled();
+  });
+
+  it('El método onClearInputs() debe resetear los campos del formualrio', () => {
+    const spyOnReset = spyOn(component.userForm, 'reset');
+
+    component.onClearInputs();
+    expect(spyOnReset).toHaveBeenCalled();
+  })
 });
